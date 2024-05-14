68203b39f99a7f760f3bb7b796fa53099723c42a
Zane <100880007+Pizzachomper@users.noreply.github.com>
2024-05-15 11:58:48 +1200

    Add files via upload

--- 00_RecipeCostsV2.py
+++ 00_RecipeCostsV2.py
@@ -74,22 +74,36 @@ def get_expenses(var_fixed):
         print()
 
         # Get name, quantity, and item
-        ingredient_name = not_blank("What is the ingredient name? ",
-                                "The ingredient name can't be blank.")
+        if var_fixed == "variable":
+            ingredient_name = not_blank("What is the ingredient name? ","The ingredient name can't be blank.")
+
+        if var_fixed == "fixed":
+            ingredient_name = not_blank("What is the name of your fixed cost? ","The name can't be blank.")
+        
         if ingredient_name.lower() == "xxx":
             break
 
-        quantity = num_check("Quantity: ",
-                            "The amount must be a number more than zero", float)
+        if var_fixed == "variable":
+            quantity = num_check("Quantity: ", "The amount must be a number more than zero", float)
         
-        units = input("What are the units for the item? ")
-                        
+        if var_fixed == "fixed":
+            quantity = 1
+
+        if var_fixed == "variable":
+            units = input("What are the units for the item? ")
+
+        if var_fixed == "fixed":
+            units = ""
         
         price = num_check("Price for the item?: $",
                             "The amount must be a number more than zero", float)
         
-        quantity_pp = num_check("What was the Quantity included in the packet / box: ",
-                            "The amount must be a number more than zero", float)
+        if var_fixed == "variable":
+            quantity_pp = num_check("What was the Quantity included in the packet / box: ",
+                                "The amount must be a number more than zero", float)
+            
+        if var_fixed == "fixed":
+            quantity_pp = 1
         
         # Add item, quantity and price to lists
         ingredient_list.append(ingredient_name)
@@ -101,16 +115,16 @@ def get_expenses(var_fixed):
     expense_frame = pandas.DataFrame(variable_dict)
 
     # Find total costs
-    var_total = expense_frame['Price'].sum()
+    sub_total = expense_frame['Price'].sum()
 
     # Currency Formatting
     add_dollars = ['Price']
     for item in add_dollars:
         expense_frame[item] = expense_frame[item].apply(currency)
 
-    quantity_costs = (var_total / quantity_pp) * quantity
+    quantity_costs = (sub_total / quantity_pp) * quantity
 
-    return [expense_frame, var_total, quantity_costs]
+    return [expense_frame, sub_total, quantity_costs]
 
 
 # Main routine goes here
@@ -134,24 +148,56 @@ variable_expenses = get_expenses("variable")
 variable_frame = variable_expenses[0]
 variable_sub = variable_expenses[1]
 quantity_costs = variable_expenses[2]
+print()
+
+have_fixed = yes_no ("Do you have fixed costs?: ")
+
+if have_fixed == "yes" or have_fixed == "y":
+    # Get fixed costs
+    fixed_expenses = get_expenses("fixed")
+    fixed_frame = fixed_expenses[0]
+    fixed_sub = fixed_expenses[1]
 
 serving_costs = quantity_costs / serving_amount  
 
-# Turn items into strings to print out in the file
+# Turn variable items into strings to print out in the file
 recipe_name_string = F"Recipe Name: {recipe_name}\nServings: {serving_amount}"
-recipe_cost_string = F"Total variable Costs of items: ${variable_sub:.2f}\nCosts based on quantity of items used: ${quantity_costs:.2f}\nCost per serving: ${serving_costs:.2f}"
-variable_frame_string = F"{variable_frame}"
+variable_frame_string = F"*** Variable table ***\n{variable_frame}"
+variable_cost_string = F"Total variable Costs of items: ${variable_sub:.2f}\nCosts based on quantity of items used: ${quantity_costs:.2f}\nCost per serving: ${serving_costs:.2f}"
 
-# Printing area
+# Variable printing area
 print()
 print(recipe_name_string)
 print()
 print(variable_frame)
 print()
-print(recipe_cost_string)
+print(variable_cost_string)
+print()
+
+if have_fixed == "yes" or have_fixed == "y":
+    
+    # Turn fixed items into strings to print out in the file
+    fixed_frame_string = F"--- Fixed table ---\n{fixed_frame}"
+    fixed_cost_string = F"Total Fixed cost of the items: ${fixed_sub:.2f}"
+    total_cost = F"Total costs (Variable + fixed): ${variable_sub + fixed_sub}"
+    
+    # Fixed printing area
+    print(fixed_frame)
+    print()
+    print(fixed_cost_string)
+    print()
+    print(total_cost)
+
+else:
+    # Create values in write to file if user dosn't use fixed costs
+    fixed_sub = 0
+    fixed_frame_string = ""
+    fixed_cost_string = ""
+    total_cost = ""
+
 
 # Write to file
-to_write = [recipe_name_string, variable_frame_string, recipe_cost_string]
+to_write = [recipe_name_string, variable_frame_string, variable_cost_string, fixed_frame_string, fixed_cost_string, total_cost]
 
 # Create file to hold data
 file_name = F"{recipe_name}.txt"
